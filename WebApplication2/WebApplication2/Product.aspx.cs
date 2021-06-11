15b9cad0ba2e183c9bb696941bb4b979b79de6b0
Andy <andy198089@yahoo.com.tw>
2021-06-11 13:56:57 +0800

    Revert "Revert "2021/06/11 余兆華""
    This reverts commit c8c15fcd0926d799c3575dacf330b0b9039a31c4.

--- WebApplication2/WebApplication2/Product.aspx.cs
+++ WebApplication2/WebApplication2/Product.aspx.cs
@@ -8,6 +8,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication2
 {
+<<<<<<< HEAD
 <<<<<<< Updated upstream
     public partial class Product : System.Web.UI.Page
     {
@@ -17,6 +18,12 @@ namespace WebApplication2
     public partial class WebForm2 : System.Web.UI.Page
     {
 >>>>>>> Stashed changes
+=======
+    public partial class Product : System.Web.UI.Page
+    {
+        public int PageCount { get; set; }
+        public int NowPage { get; set; }
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
         public class product_inf
         {
             public int ID { get; set; }
@@ -29,6 +36,7 @@ namespace WebApplication2
             List<Label> labels = new List<Label>() { Label1, Label2, Label3, Label4, Label5, Label6, Label7, Label8, Label9, Label10, Label11, Label12 };
 
             //imgae_list
+<<<<<<< HEAD
 <<<<<<< Updated upstream
             List<Image> Images = new List<Image>() { Image1, Image2, Image3, Image4, Image5, Image6,
                 Image7, Image8, Image9 , Image10, Image11, Image12 };
@@ -36,6 +44,10 @@ namespace WebApplication2
             List<ImageButton> imageButtons = new List<ImageButton>() { ImageButton1, ImageButton2, ImageButton3, ImageButton4, ImageButton5, ImageButton6, 
                 ImageButton7, ImageButton8, ImageButton9 , ImageButton10, ImageButton11, ImageButton12 };
 >>>>>>> Stashed changes
+=======
+            List<Image> Images = new List<Image>() { Image1, Image2, Image3, Image4, Image5, Image6,
+                Image7, Image8, Image9 , Image10, Image11, Image12 };
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
             //product_information_list
             List<product_inf> product_infs = new List<product_inf>();
 
@@ -63,7 +75,10 @@ namespace WebApplication2
                         product_type = sqlDataReader["型號"].ToString(),
                         product_name = sqlDataReader["名稱"].ToString(),
                     };
+<<<<<<< HEAD
 <<<<<<< Updated upstream
+=======
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
                     if (product_infs.Count == 0)
                     {
                         product_infs.Add(product_inf);
@@ -80,14 +95,20 @@ namespace WebApplication2
                             product_infs.Add(product_inf);
                         }
                     }
+<<<<<<< HEAD
 =======
                     product_infs.Add(product_inf);                    
 >>>>>>> Stashed changes
+=======
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
                 }
             }
             sqlConnection.Close();
 
+<<<<<<< HEAD
 <<<<<<< Updated upstream
+=======
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
             //分頁設置
             if(product_infs.Count % 12 == 0)
             {
@@ -102,6 +123,7 @@ namespace WebApplication2
             NowPage = pageIndex;
 
             int j = 12 * (pageIndex-1);
+<<<<<<< HEAD
             //將資訊塞入
             for (int i = 0; i < 12;i++)
             {
@@ -115,19 +137,26 @@ namespace WebApplication2
                     labels[i].Text = product_infs[i+j].product_type+ " "+ product_infs[i+j].product_name;
                     Images[i].ImageUrl = $"~/product_pic/{product_infs[i+j].product_type}_2.jpg";
 =======
+=======
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
             //將資訊塞入
-            for(int i = 0; i < 12;i++)
+            for (int i = 0; i < 12;i++)
             {
-                if(i >= product_infs.Count)
+                if(i >= product_infs.Count-j)
                 {
                     labels[i].Text = "";
-                    imageButtons[i].ImageUrl = "";
+                    Images[i].ImageUrl = "";
                 }
                 else
                 {
+<<<<<<< HEAD
                     labels[i].Text = product_infs[i].product_type+ " "+ product_infs[i].product_name;
                     imageButtons[i].ImageUrl = $"~/product_pic/{product_infs[i].product_type}_2.jpg";
 >>>>>>> Stashed changes
+=======
+                    labels[i].Text = product_infs[i+j].product_type+ " "+ product_infs[i+j].product_name;
+                    Images[i].ImageUrl = $"~/product_pic/{product_infs[i+j].product_type}_2.jpg";
+>>>>>>> 2efcb562025d4e8f6649e146e45494bbc45b2805
                 }
             }
         }
