3557f29bdd18b5bc7d4d05e855d0a236a9b456f9
Andy <andy198089@yahoo.com.tw>
2021-06-16 19:04:42 +0800

    2021/6/16 by 晉文
    新增 管理者頁面(未完成全部功能)

---
+++ WebApplication2/WebApplication2/ManagerPage.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="ManagerPage" Language="C#" MasterPageFile="~/index.Master" AutoEventWireup="true" CodeBehind="ManagerPage.aspx.cs" Inherits="WebApplication2.ManagerPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <title>ManagerPage</title>
+
+    <asp:Label ID="Label1" runat="server" Text="Manager Page" ForeColor="Black" Font-Size="X-Large"></asp:Label><br />
+    <asp:Label ID="Label2" runat="server" Text="商品總覽" ForeColor="Black" Font-Size="Large"></asp:Label><br />
+    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList><br />
+    <asp:Label ID="Label3" runat="server" Text=" " Font-Size="Larger"></asp:Label><br />
+    <div>
+        <asp:GridView ID="GridView1" runat="server" OnRowDataBound="GridView1_RowDataBound" OnPageIndexChanging="GridView1_PageIndexChanging" AllowPaging="true" PageSize="15" PagerSettings-Mode="Numeric">
+            <PagerTemplate>
+                <table>
+                    <tr>
+                        <td style="text-align: right">第&nbsp;<asp:Label ID="lblPageIndex" runat="server" Text="<%#((GridView)Container.Parent.Parent).PageIndex + 1 %>" ForeColor="Blue"></asp:Label>&nbsp;頁，
+                        共&nbsp;<asp:Label ID="lblPageCount" runat="server" Text="<%# ((GridView)Container.Parent.Parent).PageCount %>" ForeColor="Blue"></asp:Label>&nbsp;頁，
+                        <asp:LinkButton ID="btnFirst" runat="server" CausesValidation="False" CommandArgument="First" CommandName="Page" Text="第一頁" CssClass="btn btn-primary btn-xs"></asp:LinkButton>&nbsp;&nbsp;
+                        <asp:LinkButton ID="btnPrev" runat="server" CausesValidation="False" CommandArgument="Prev" CommandName="Page" Text="上一頁" CssClass="btn btn-primary btn-xs"></asp:LinkButton>&nbsp;&nbsp;
+                        <asp:LinkButton ID="btnNext" runat="server" CausesValidation="False" CommandArgument="Next" CommandName="Page" Text="下一頁" CssClass="btn btn-primary btn-xs"></asp:LinkButton>&nbsp;&nbsp;
+                        <asp:LinkButton ID="btnLast" runat="server" CausesValidation="False" CommandArgument="Last" CommandName="Page" Text="最後一頁" CssClass="btn btn-primary btn-xs"></asp:LinkButton>&nbsp;&nbsp;
+                        跳到第<asp:TextBox ID="txtNewPageIndex" runat="server" Text="<%# ((GridView)Container.Parent.Parent).PageIndex + 1%>" Width="100px"></asp:TextBox>頁&nbsp;
+                        每頁顯示<asp:TextBox ID="TextBox_PageSize" runat="server" Text="<%# ((GridView)Container.Parent.Parent).PageSize %>" Width="100px"></asp:TextBox>筆&nbsp;
+                        <asp:LinkButton ID="btnGo" runat="server" CausesValidation="False" CommandArgument="-1" CommandName="Page" Text="GO" CssClass="btn btn-primary btn-xs"></asp:LinkButton>&nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </PagerTemplate>
+        </asp:GridView>
+    </div>
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="新增商品" OnClick="Button1_Click" /><asp:Button ID="Button2" runat="server" Text="更新商品" OnClick="Button2_Click" /><br />
+    <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label><br />
+</asp:Content>
---
+++ WebApplication2/WebApplication2/ManagerPage.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication2
+{
+    public partial class ManagerPage : System.Web.UI.Page
+    {
+        public List<string> datanames = new List<string>() { "請選擇" };
+
+        //dataname_list(內部用)
+        public List<string> datanames_1 = new List<string>() { };
+        public int Idx;
+        public void BindGrid(int idx)  //商品呈現於Grid表格中
+        {
+            string sql_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ProductsConnectionString"].ConnectionString;
+            SqlConnection sqlConnection_2 = new SqlConnection(sql_data);
+            if (idx < 0)
+            {
+                GridView1.DataSource = null;
+                GridView1.DataBind();
+            }
+            else
+            {
+                string product_class = datanames_1[idx]; //取得對應index的商品系列
+                string product_list = $"[{product_class}]";
+                string sqlstr = $"select * from {product_list}";
+                SqlCommand sqlCommand_2 = new SqlCommand(sqlstr, sqlConnection_2);
+                sqlConnection_2.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(sqlCommand_2);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            sqlConnection_2.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string sql_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ProductsConnectionString"].ConnectionString;
+
+            //抓取所有資料表名稱
+            string sql_dataname = "select Table_name from INFORMATION_SCHEMA.TABLES order by Table_name";
+
+            SqlConnection sqlConnection = new SqlConnection(sql_data);
+
+            SqlCommand sqlCommand = new SqlCommand(sql_dataname, sqlConnection);
+
+            sqlConnection.Open();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            if (sqlDataReader.HasRows)
+            {
+                while (sqlDataReader.Read())
+                {
+                    datanames.Add(sqlDataReader["Table_name"].ToString().Replace("$", "").Replace("'", "").Replace("#", "。"));
+                    datanames_1.Add(sqlDataReader["Table_name"].ToString());
+                }
+            }
+            sqlConnection.Close();
+
+            if (!IsPostBack) //防止重複刷新==>造成只能選取第一項
+            {
+                //商品系列填入下拉式選單
+                this.DropDownList1.DataSource = datanames;
+                this.DropDownList1.DataBind();
+                this.DropDownList1.Style["display"] = "none"; //將舊樣式的隱藏起來
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = DropDownList1.SelectedIndex - 1; //取得對應的index
+            Idx = idx;
+            BindGrid(idx);
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            DataRowView drv = (DataRowView)e.Row.DataItem;
+
+            if ((e.Row.RowType == DataControlRowType.DataRow) || (e.Row.RowType == DataControlRowType.Footer))
+            {
+                if (drv != null)
+                {
+                    if ((int)drv["數量"] < 25)  //當商品數量低於25個
+                    {
+                        e.Row.ForeColor = System.Drawing.Color.Red; //符合 => 背景改紅色警示
+                        Label3.Text = "(注意!!!有庫存過低的商品!!)";
+                        Label3.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        e.Row.ForeColor = System.Drawing.Color.Black;
+                    }
+                }
+            }
+            
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView gvw = (GridView)sender;
+            TextBox TextBox_PageSize = (TextBox)gvw.BottomPagerRow.FindControl("TextBox_PageSize");
+            
+
+            if (e.NewPageIndex < 0)
+            {
+                TextBox pageNum = (TextBox)gvw.BottomPagerRow.FindControl("txtNewPageIndex");
+                int Pa = int.Parse(pageNum.Text);
+                if (Pa <= 0)
+                {
+                    gvw.PageIndex = 0;
+                }
+                else
+                {
+                    gvw.PageIndex = Pa - 1;
+                }
+            }
+            else
+            {
+                gvw.PageIndex = e.NewPageIndex;
+            }
+            BindGrid(Idx);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/AddProduct.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
