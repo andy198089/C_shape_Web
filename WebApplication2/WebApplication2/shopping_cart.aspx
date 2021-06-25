08f24f5e789cf963eafcd884db97b0a341bda079
YU-JIAO-HUA <one445d7@gmail.com>
2021-06-25 17:34:51 +0800

    2021/06/25 余兆華
    增加購物車選項&細節修正

--- WebApplication2/WebApplication2/shopping_cart.aspx
+++ WebApplication2/WebApplication2/shopping_cart.aspx
@@ -2,6 +2,6 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <asp:Label ID="Label1" runat="server" Text="購物車" Font-Size="X-Large"></asp:Label>
     <asp:GridView ID="GridView1" runat="server"></asp:GridView>
-    <asp:Label ID="Label2" runat="server" Text="Label" Visible="False"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="Label" Visible="False"></asp:Label><br />
     <INPUT type="button" value="繼續購物" onclick='history.go(-<%= (int)ViewState["submittimes"] %>)'>-<asp:Button ID="Button2" runat="server" Text="提交訂單" OnClick="Button2_Click" />
 </asp:Content>
--- WebApplication2/WebApplication2/shopping_cart.aspx.cs
+++ WebApplication2/WebApplication2/shopping_cart.aspx.cs
@@ -21,7 +21,6 @@ namespace WebApplication2
         string large = "";
         int price = 0;
         int sum = 0;
-        int row = 0;
         string[] buyList = { };
         int count = 0;
         protected void Page_Load(object sender, EventArgs e)
@@ -34,72 +33,79 @@ namespace WebApplication2
             }
             if (Session["Logined"] == "login")
             {
-                string sql_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ProductsConnectionString"].ConnectionString;
-
-                SqlConnection sqlConnection = new SqlConnection(sql_data);
-
-                buyList = Session["buyorder"].ToString().Split(',');
-
-                DataTable dt = new DataTable();
-                dt.Columns.Add("ID");
-                dt.Columns.Add("系列");
-                dt.Columns.Add("型號");
-                dt.Columns.Add("名稱");
-                dt.Columns.Add("規格");
-                dt.Columns.Add("尺寸");
-                dt.Columns.Add("數量");
-                dt.Columns.Add("售價");
-                dt.Columns.Add("總價");
-
-                foreach (var buy in buyList)
+                if (Session["buyorder"] != null)
                 {
-                    DataRow dr = dt.NewRow();
-                    count++;
-                    if (count % 3 == 1)
+                    string sql_data = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["ProductsConnectionString"].ConnectionString;
+
+                    SqlConnection sqlConnection = new SqlConnection(sql_data);
+
+                    buyList = Session["buyorder"].ToString().Split(',');
+
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("ID");
+                    dt.Columns.Add("系列");
+                    dt.Columns.Add("型號");
+                    dt.Columns.Add("名稱");
+                    dt.Columns.Add("規格");
+                    dt.Columns.Add("尺寸");
+                    dt.Columns.Add("數量");
+                    dt.Columns.Add("售價");
+                    dt.Columns.Add("總價");
+
+                    foreach (var buy in buyList)
                     {
-                        tableName = buy;
-                    }
-                    if (count % 3 == 2)
-                    {
-                        string sqlstr_products = "select * from [" + tableName + "] where ID = " + buy;
-
-                        SqlCommand sqlCommand = new SqlCommand(sqlstr_products, sqlConnection);
-
-                        sqlConnection.Open();
-
-                        SqlDataReader sqlDataReader2 = sqlCommand.ExecuteReader();
-
-                        if (sqlDataReader2.HasRows)
+                        DataRow dr = dt.NewRow();
+                        count++;
+                        if (count % 3 == 1)
                         {
-                            if (sqlDataReader2.Read())
-                            {
-                                itemID = Convert.ToInt32(sqlDataReader2["ID"]);
-                                model = sqlDataReader2["型號"].ToString();
-                                productName = sqlDataReader2["名稱"].ToString();
-                                size = sqlDataReader2["規格"].ToString();
-                                large = sqlDataReader2["尺寸"].ToString();
-                                price = Convert.ToInt32(sqlDataReader2["售價"]);
-                            }
+                            tableName = buy;
                         }
-                        sqlConnection.Close();
+                        if (count % 3 == 2)
+                        {
+                            string sqlstr_products = "select * from [" + tableName + "] where ID = " + buy;
+
+                            SqlCommand sqlCommand = new SqlCommand(sqlstr_products, sqlConnection);
+
+                            sqlConnection.Open();
+
+                            SqlDataReader sqlDataReader2 = sqlCommand.ExecuteReader();
+
+                            if (sqlDataReader2.HasRows)
+                            {
+                                if (sqlDataReader2.Read())
+                                {
+                                    itemID = Convert.ToInt32(sqlDataReader2["ID"]);
+                                    model = sqlDataReader2["型號"].ToString();
+                                    productName = sqlDataReader2["名稱"].ToString();
+                                    size = sqlDataReader2["規格"].ToString();
+                                    large = sqlDataReader2["尺寸"].ToString();
+                                    price = Convert.ToInt32(sqlDataReader2["售價"]);
+                                }
+                            }
+                            sqlConnection.Close();
+                        }
+                        if (count % 3 == 0)
+                        {
+                            sum = Convert.ToInt32(buy);
+                            dr["ID"] = itemID;
+                            dr["系列"] = tableName.Replace("$", "").Replace("'", "");
+                            dr["型號"] = model;
+                            dr["名稱"] = productName;
+                            dr["規格"] = size;
+                            dr["尺寸"] = large;
+                            dr["數量"] = sum;
+                            dr["售價"] = price;
+                            dr["總價"] = sum * price;
+                            dt.Rows.Add(dr);
+                            GridView1.DataSource = dt;
+                            GridView1.DataBind();
+                        }
                     }
-                    if (count % 3 == 0)
-                    {
-                        sum = Convert.ToInt32(buy);
-                        dr["ID"] = itemID;
-                        dr["系列"] = tableName.Replace("$", "").Replace("'", "");
-                        dr["型號"] = model;
-                        dr["名稱"] = productName;
-                        dr["規格"] = size;
-                        dr["尺寸"] = large;
-                        dr["數量"] = sum;
-                        dr["售價"] = price;
-                        dr["總價"] = sum * price;
-                        dt.Rows.Add(dr);
-                        GridView1.DataSource = dt;
-                        GridView1.DataBind();
-                        row++;
-                    }
+                }
+                else
+                {
+                    Label2.Visible = true;
+                    Label2.Text = "你的購物車裡沒有商品...";
                 }
             }
             else
